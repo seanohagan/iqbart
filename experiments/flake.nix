de86e04fcded396911fb8aa84b6fbe5b3a9e1c31
seanohagan <sean@seanohagan.com>
2025-09-22 15:30:48 -0500

    update dependencies in flake and pyproject to work across aarch64 and linux x86-64

--- experiments/flake.nix
+++ experiments/flake.nix
@@ -1,14 +1,11 @@
-# flake.nix (Final Simplified Version)
 {
   description = "A development environment using uv for Python dependencies.";
 
   inputs = {
-    # We only need one modern nixpkgs input now.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # The outputs are simple again, no legacy input.
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -17,6 +14,9 @@
           pname = "iqbart-cpp";
           version = "0.1.0";
           src = pkgs.lib.cleanSource ../iqbart;
+          unpackPhase = ''
+            cp -r $src/. ./
+          '';
           nativeBuildInputs = [ pkgs.cmake pkgs.python311Packages.pybind11];
           buildInputs = [pkgs.python311 ];
           installPhase = ''
@@ -27,26 +27,63 @@
         iqbartPythonPath = "${iqbart-cpp}/${pkgs.python311.sitePackages}";
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.python311
-            pkgs.uv
-            pkgs.R
-            pkgs.gcc
-            pkgs.git
-            pkgs.llvm
-          ];
+        devShells.default =
+          let
+            drbart = pkgs.rPackages.buildRPackage {
+              name="drbart";
+              version="0.0.0.9";
+              src = pkgs.fetchFromGitHub {
+                  owner = "vittorioorlandi";
+                  repo = "drbart";
+                  rev = "main";
 
-          shellHook = ''
-            echo "Nix environment activated. Setting up Python virtual environment with uv..."
-            test -d .venv || uv venv
-            source .venv/bin/activate
-            uv pip install -e . --quiet
-            echo "Done. Python environment is ready."
-            export PYTHONPATH="${iqbartPythonPath}:$PYTHONPATH"
-            echo "Custom C++ module 'iqbart_cpp' is now available to Python."
-          '';
-        };
+                  sha256 = "sha256-D/ft7CkUiiBx+218rWy354IFsfHdAx2rxRyR/ENvn8A=";
+                };
+
+              unpackPhase = ''
+                cp -r $src/. ./
+                chmod -R u+w .
+              '';
+
+              nativeBuildInputs = [ pkgs.gfortran pkgs.gfortran.cc ];
+
+                propagatedBuildInputs = with pkgs.rPackages; [
+                  Rcpp
+                  RColorBrewer
+                ];
+            };
+          in
+            pkgs.mkShell {
+              buildInputs = [
+                pkgs.python311
+                pkgs.uv
+                pkgs.R
+                pkgs.gcc
+                pkgs.git
+                pkgs.llvm
+                pkgs.stdenv.cc.cc.lib
+                pkgs.bashInteractive
+                pkgs.rPackages.gamlss
+                pkgs.gfortran
+                pkgs.gfortran.cc
+
+                drbart
+              ];
+
+              #export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+
+              shellHook = ''
+                echo "Nix environment activated. Setting up Python virtual environment with uv..."
+                test -d .venv || uv venv
+                source .venv/bin/activate
+                uv pip install -e . --quiet --no-binary=true
+                echo "Done. Python environment is ready."
+                export LD_LIBRARY_PATH="$(python -m rpy2.situation LD_LIBRARY_PATH)":$LD_LIBRARY_PATH
+                export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+                export PYTHONPATH="${iqbartPythonPath}:$PYTHONPATH"
+                echo "Custom C++ module 'iqbart_cpp' is now available to Python."
+              '';
+            };
       }
     );
 }
